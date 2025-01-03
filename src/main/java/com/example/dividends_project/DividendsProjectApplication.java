a3dd2603bcb392114f4d51dbe5638c7794ac7bab
Ho-JunYu <hojoon1011@gmail.com>
2025-01-03 19:25:26 +0900

    first commit

---
+++ src/main/java/com/example/dividends_project/DividendsProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.dividends_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DividendsProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DividendsProjectApplication.class, args);
+    }
+
+}
